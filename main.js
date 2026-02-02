860db0a9a0d96ab794737746d5c3bbe662de1a03
Surasek Nusati <surasek@gmail.com>
2026-02-03 06:37:51 +0700

    refactor: improve mobile solution

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 
 const shareBtns = document.querySelectorAll('.article__byline-share-btn');
-const shareMobile = document.querySelector('.share.share--inline');
-const articleByline = document.querySelector('.article__byline');
+const article = document.querySelector('.article');
 
 shareBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        shareMobile.classList.toggle("is-hidden");
-        articleByline.classList.toggle("is-hidden");
+        article.classList.toggle("is-share-open");
     });
 });
