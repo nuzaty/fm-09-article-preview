b354d17fac47616cb065d12e79029386a90565ce
Surasek Nusati <surasek@gmail.com>
2026-02-03 05:14:55 +0700

    fix(ui): improve mobile solution

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-let shareBtns = document.querySelectorAll('.article__byline-share-btn');
-let shareMobile = document.querySelector('.share.share--inline');
-let articleByline = document.querySelector('.article__byline');
+const shareBtns = document.querySelectorAll('.article__byline-share-btn');
+const shareMobile = document.querySelector('.share.share--inline');
+const articleByline = document.querySelector('.article__byline');
 
-shareBtns.forEach(shareBtn => {
-    shareBtn.addEventListener('click', () => {
+shareBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
         shareMobile.classList.toggle("is-hidden");
         articleByline.classList.toggle("is-hidden");
     });
